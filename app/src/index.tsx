26010fa31d014437a6790edd7ce8b17713eaa2f0
Joan López de la Franca Beltran <joanjan14@gmail.com>
2023-03-17 14:08:37 +0100

    Sync main with thema/wasm

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {LoadWasm} from "./wasm/index";
+import {ThemeProvider} from './theme';
+import {StateProvider} from './state';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const root: ReactDOM.Root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-      <LoadWasm>
-          <App />
-      </LoadWasm>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider>
+            <StateProvider>
+                <App/>
+            </StateProvider>
+        </ThemeProvider>
+    </React.StrictMode>
 );
+
+// Now load and run the Go code which will register the Wasm API
+const go: Go = new Go();
+
+const sourceFile = 'play-thema/thema.wasm';
+
+// WebAssembly.instantiateStreaming() is preferred, but not all browsers support it
+if (typeof WebAssembly.instantiateStreaming === 'function') {
+    WebAssembly.instantiateStreaming(fetch(sourceFile), go.importObject).then(result => {
+        go.run(result.instance);
+    });
+} else {
+    fetch(sourceFile).then(response =>
+        response.arrayBuffer()
+    ).then(bytes =>
+        WebAssembly.instantiate(bytes, go.importObject)
+    ).then(result => {
+        go.run(result.instance);
+    });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
