b625c88e2ed348976a66ade70a35b3b58d15c1ab
Clarity-89 <homes89@ukr.net>
2023-05-12 18:33:47 +0300

    Fix column height

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,25 @@ import { StateContext } from './state';
 import { GrafanaTheme2 } from '@grafana/data';
 import { css } from '@emotion/css';
 import { useStyles } from './theme';
-import { useTheme2 } from '@grafana/ui';
 
 const App = () => {
   const styles = useStyles(getStyles);
-  const theme = useTheme2();
   const { input, lineage, setInput, setLineage } = useContext(StateContext);
 
   return (
     <div className={styles.container}>
       <Header />
-      <Column title="LINEAGE (CUE)" color="green">
-        <CodeEditor value={lineage} language="go" onChange={(lineage?: string) => setLineage(lineage || '')} />
-      </Column>
-      <Column title="INPUT DATA (JSON)" color="green">
-        <CodeEditor value={input} language="json" onChange={(input?: string) => setInput(input || '')} />
-      </Column>
-      <Column title="OUTPUT" color="darkblue">
-        <Console />
-      </Column>
+      <div className={styles.wrapper}>
+        <Column title="LINEAGE (CUE)" color="green">
+          <CodeEditor value={lineage} language="go" onChange={(lineage?: string) => setLineage(lineage || '')} />
+        </Column>
+        <Column title="INPUT DATA (JSON)" color="green">
+          <CodeEditor value={input} language="json" onChange={(input?: string) => setInput(input || '')} />
+        </Column>
+        <Column title="OUTPUT" color="darkblue">
+          <Console />
+        </Column>
+      </div>
     </div>
   );
 };
@@ -34,11 +34,17 @@ const getStyles = (theme: GrafanaTheme2) => {
   return {
     container: css`
       display: flex;
+      flex-direction: column;
       flex-wrap: wrap;
       background: ${theme.colors.background.primary};
-      height: 100vh;
       text-align: center;
       font-family: Inter, Helvetica, Arial, sans-serif;
+      min-height: 100vh;
+      height: 100%;
+    `,
+    wrapper: css`
+      display: flex;
+      margin-top: ${theme.spacing(3)};
     `,
   };
 };
